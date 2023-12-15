52de51736505bcf98d18f871878b97b0f18f1e76
rickie-austin-114 <rcya996001@gmail.com>
2023-12-15 14:41:26 +0800

    Updated client side by Rickie

--- client/src/components/authentications/AdminLogin.jsx
+++ client/src/components/authentications/AdminLogin.jsx
@@ -8,7 +8,7 @@ const AdminLogin = () => {
             <div class="container p-10 my-10 border bg-info">
                 <div class="container p-5 my-5 border bg-white">
                     <h1>User login</h1>
-                    <form action="https://backendadmin.com" method="POST" id="loginform">
+                    <form action="http://localhost:3000/login" method="POST" id="loginform">
                         <input type="text" name="username" id="username" placeholder="Username" /><br />
                         <input type="password" name="password" id="password" placeholder="password" /><br />
                         <input type="submit" /><i class="bi bi-file-person"></i>
--- client/src/components/authentications/UserLogin.jsx
+++ client/src/components/authentications/UserLogin.jsx
@@ -10,7 +10,7 @@ const UserLogin = () => {
             <div class="container p-10 my-10 border bg-info">
                 <div class="container p-5 my-5 border bg-white">
                     <h1>User login</h1>
-                    <form action="https://backend.com" method="POST" id="loginform">
+                    <form action="https://localhost:3000/login" method="POST" id="loginform">
                         <input type="text" name="username" id="username" placeholder="Username" /><br />
                         <input type="password" name="password" id="password" placeholder="password" /><br />
                         <div class="g-signin2" data-onsuccess="onSignIn"></div><input type="submit" /><i class="bi bi-file-person"></i>
